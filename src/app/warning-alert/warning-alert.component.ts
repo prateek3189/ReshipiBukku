eb3e828dddc11d2a00c58fe6414459b54527d950
prateek-tavisca <pmagarde@tavisca.com>
2019-08-31 18:24:21 +0530

    Components String Interpolation Property Binding

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert.html',
+    styleUrls: ['./warning-alert.scss']
+})
+
+export class WarningAlertComponent {
+
+}
